dfb9bc65d891385f12641c4516111eeb1fa80855
lebed <lebed@DESKTOP-BKIICKL>
2022-07-06 14:52:47 +0300

    Add project files.

---
+++ httpRequests.cpp
@@ -0,0 +1,12 @@
+﻿#include "stdafx.h"
+#include"cpr/cpr.h"
+#include<iostream>
+int main()
+{
+    // Проверка работоспособности http-доступа на сайте https://www.httpbin.org/get
+    auto response = cpr::Get(cpr::Url{ "https://www.httpbin.org/get" },
+        cpr::Parameters{ { "hello", "world" } },
+        cpr::VerifySsl(false));
+    std::cout << response.text << std::endl;
+    return 0;
+}
